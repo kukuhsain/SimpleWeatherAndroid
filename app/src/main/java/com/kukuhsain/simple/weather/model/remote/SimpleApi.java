d74ed18d3492c18c3c3d40f3930b8d2beaf0cf18
Kukuh <kukuhsain@gmail.com>
2016-11-17 05:54:13 +0700

    Add new API of getting forecast by city

--- app/src/main/java/com/kukuhsain/simple/weather/model/remote/SimpleApi.java
+++ app/src/main/java/com/kukuhsain/simple/weather/model/remote/SimpleApi.java
@@ -1,10 +1,14 @@
 package com.kukuhsain.simple.weather.model.remote;
 
 import com.kukuhsain.simple.weather.model.local.PreferencesHelper;
+import com.kukuhsain.simple.weather.model.pojo.ForecastResult;
 
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by kukuh on 15/10/16.
@@ -12,6 +16,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SimpleApi {
     public static final String BASE_URL = "http://api.openweathermap.org";
+    private final String OPENWEATHER_APP_ID = "cef209f6f46bb527dba385b81b808ce9";
     private static SimpleApi INSTANCE;
     private ApiEndpoint api;
     private static String accessToken;
@@ -34,7 +39,16 @@ public class SimpleApi {
         return INSTANCE;
     }
 
-    private interface ApiEndpoint {
+    public Observable<ForecastResult> getForecastByCity(String city) {
+        return api.getForecastByCity(city, "json", "metric", "16", OPENWEATHER_APP_ID);
+    }
 
+    private interface ApiEndpoint {
+        @GET("/data/2.5/forecast/daily")
+        Observable<ForecastResult> getForecastByCity(@Query("q") String city,
+                                                     @Query("mode") String mode,
+                                                     @Query("units") String units,
+                                                     @Query("cnt") String cnt,
+                                                     @Query("appid") String appId);
     }
 }
