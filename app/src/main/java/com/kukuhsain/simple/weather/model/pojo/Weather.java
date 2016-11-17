39c4878692fb9c82bd2fb8b779d4dca796b9c39a
Kukuh <kukuhsain@gmail.com>
2016-11-17 07:43:42 +0700

    Update Weather pojo class

--- app/src/main/java/com/kukuhsain/simple/weather/model/pojo/Weather.java
+++ app/src/main/java/com/kukuhsain/simple/weather/model/pojo/Weather.java
@@ -1,46 +1,74 @@
 package com.kukuhsain.simple.weather.model.pojo;
 
-import io.realm.RealmObject;
-
 /**
  * Created by kukuh on 14/11/16.
  */
 
-public class Weather extends RealmObject {
-    private long sampleId;
+public class Weather {
     private String name;
     private String description;
+    private String icon;
 
-    public Weather() {
-    }
+    private String city;
+    private String countryId;
 
-    public Weather(long sampleId, String name, String description) {
-        this.sampleId = sampleId;
+    private float cloudiness;
+    private float pressure;
+    private float humidity;
+
+    private double latitude;
+    private double longitude;
+
+    public Weather(String name, String description, String icon, String city, String countryId, float cloudiness, float pressure, float humidity, double latitude, double longitude) {
         this.name = name;
         this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+        this.icon = icon;
+        this.city = city;
+        this.countryId = countryId;
+        this.cloudiness = cloudiness;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getDescription() {
+        return description;
     }
 
-    public double getSampleId() {
-        return sampleId;
+    public String getIcon() {
+        return icon;
     }
 
-    public void setSampleId(long sampleId) {
-        this.sampleId = sampleId;
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public float getCloudiness() {
+        return cloudiness;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
     }
 }
