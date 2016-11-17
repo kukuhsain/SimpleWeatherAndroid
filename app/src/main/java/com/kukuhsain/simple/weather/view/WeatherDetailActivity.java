6e5cf5318b5eacfc86784014cd6d0e475e060a53
Kukuh <kukuhsain@gmail.com>
2016-11-18 06:11:09 +0700

    Update UI in weather detail, and edit theme

--- app/src/main/java/com/kukuhsain/simple/weather/view/WeatherDetailActivity.java
+++ app/src/main/java/com/kukuhsain/simple/weather/view/WeatherDetailActivity.java
@@ -3,11 +3,14 @@ package com.kukuhsain.simple.weather.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.google.gson.Gson;
 import com.kukuhsain.simple.weather.R;
 import com.kukuhsain.simple.weather.model.pojo.Weather;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import timber.log.Timber;
 
 /**
@@ -15,15 +18,23 @@ import timber.log.Timber;
  */
 
 public class WeatherDetailActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_detail);
+        ButterKnife.bind(this);
 
         String weatherString = getIntent().getStringExtra("weather");
         Weather weather = (new Gson()).fromJson(weatherString, Weather.class);
 
         Timber.d("weather received...");
-        Timber.d(weather.getName());
+        if (weather != null) {
+            Timber.d(weather.getName());
+        }
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Weather Detail");
     }
 }
